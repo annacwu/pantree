ef879982b3ed54bb04f01fd56b33a6900b7fb51e
annacwu <annacwu04@gmail.com>
2024-01-17 22:18:33 -0800

    Initial Commit

---
+++ pantree/pantreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  pantreeApp.swift
+//  pantree
+//
+//  Created by Anna Wu on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct pantreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
