d1fec1739a4f5a703a4bd5f0e79d29fa4d05f0f3
annacwu <annacwu04@gmail.com>
2024-01-22 11:51:25 -0800

    integrating leaves in content view

--- pantree/ContentView.swift
+++ pantree/ContentView.swift
@@ -9,22 +9,40 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Query var branches: [Branch]
+//    @Query var branches: [Branch]
+    @Query var leaves: [Leaf]
     @Environment(\.modelContext) var modelContext
     
+    @State private var showingAddScreen = false
+    
     var body: some View {
-        NavigationStack{
-            List(branches) { branch in
-                Text(branch.name)
-            }
-            .navigationTitle("Branches")
-            .toolbar{
-                Button("Add"){
-                    let branch = Branch(name: "anna", type: "admin")
-                    modelContext.insert(branch)
+        NavigationStack {
+            Text("Count: \(leaves.count)")
+                .navigationTitle("pantree")
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button("Add leaf", systemImage: "plus"){
+                            showingAddScreen.toggle()
+                        }
+                    }
                 }
-            }
-            }
+                .sheet(isPresented: $showingAddScreen) {
+                    AddLeafView()
+                }
+        }
+        
+//        NavigationStack{
+//            List(branches) { branch in
+//                Text(branch.name)
+//            }
+//            .navigationTitle("Branches")
+//            .toolbar{
+//                Button("Add"){
+//                    let branch = Branch(name: "anna", type: "admin")
+//                    modelContext.insert(branch)
+//                }
+//            }
+//            }
         }
     }
 
