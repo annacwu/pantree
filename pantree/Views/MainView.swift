4517e74763afb9684172960d36a3acbd9302d97e
annacwu <annacwu04@gmail.com>
2024-02-05 11:45:15 -0800

    add pantry view

--- pantree/Views/MainView.swift
+++ pantree/Views/MainView.swift
@@ -14,7 +14,7 @@ struct MainView: View {
     var body: some View {
         if viewModel.isSignedIn, !viewModel.currentUserId.isEmpty {
             // signed in
-            
+            PantryView()
         } else {
             LoginView()
         }
---
+++ pantree/Views/PantryView.swift
@@ -0,0 +1,18 @@
+//
+//  PantryView.swift
+//  pantree
+//
+//  Created by Anna Wu on 2/5/24.
+//
+
+import SwiftUI
+
+struct PantryView: View {
+    var body: some View {
+        Text("welcome to your pantry.")
+    }
+}
+
+#Preview {
+    PantryView()
+}
