e1d2501e58ba6aa0b0378fca07b28e7bdf94d11c
annacwu <annacwu04@gmail.com>
2024-02-09 12:15:45 -0800

    add save item to db functionality

--- pantree/Models/Leaf.swift
+++ pantree/Models/Leaf.swift
@@ -8,17 +8,29 @@
 import Foundation
 import SwiftData
 
-@Model
-class Leaf {
-    var name: String
-    var date: String
-    var low: String
-    var user: String
+struct LeafItem: Codable, Identifiable {
+    let id: String
+    let title: String
+    let dateAdded: TimeInterval
+    let createdDate: TimeInterval
+    var isGone: Bool
     
-    init(name: String, date: String, low: String, user: String) {
-        self.name = name
-        self.date = date
-        self.low = low
-        self.user = user
+    mutating func setGone(_ state: Bool) {
+        isGone = state
     }
 }
+
+//@Model
+//class Leaf {
+//    var name: String
+//    var date: String
+//    var low: String
+//    var user: String
+//    
+//    init(name: String, date: String, low: String, user: String) {
+//        self.name = name
+//        self.date = date
+//        self.low = low
+//        self.user = user
+//    }
+//}
--- pantree/ViewModels/AddLeafViewViewModel.swift
+++ pantree/ViewModels/AddLeafViewViewModel.swift
@@ -4,7 +4,8 @@
 //
 //  Created by Anna Wu on 2/7/24.
 //
-
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 class AddLeafViewViewModel: ObservableObject {
@@ -15,7 +16,32 @@ class AddLeafViewViewModel: ObservableObject {
     init() {}
     
     func save() {
+        guard canSave else {
+            return
+        }
         
+        // get current user id
+        guard let uId = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        // create model
+        let newId = UUID().uuidString
+        let newLeaf = LeafItem(
+            id: newId,
+            title: title,
+            dateAdded: dateAdded.timeIntervalSince1970,
+            createdDate: Date().timeIntervalSince1970,
+            isGone: false)
+        
+        // save model
+        let db = Firestore.firestore()
+        
+        db.collection("users")
+            .document(uId)
+            .collection("pantry")
+            .document("123")
+            .setData(newLeaf.asDictionary())
     }
     
     var canSave: Bool {
