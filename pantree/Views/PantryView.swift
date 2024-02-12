77a792291c928ced2dd13a4dbffb818588834345
annacwu <annacwu04@gmail.com>
2024-02-12 10:57:18 -0800

    Create ui list from database wrapper

--- pantree/Views/PantryView.swift
+++ pantree/Views/PantryView.swift
@@ -4,22 +4,26 @@
 //
 //  Created by Anna Wu on 2/5/24.
 //
-
+import FirebaseFirestoreSwift
 import SwiftUI
 
 struct PantryView: View {
     @StateObject var viewModel = PantryViewViewModel()
-    
-    private let userId: String
+    @FirestoreQuery var items: [LeafItem]
     
     init(userId: String) {
-        self.userId = userId
+        self._items = FirestoreQuery(
+            collectionPath: "users/\(userId)/pantry"
+        )
     }
     
     var body: some View {
         NavigationView {
             VStack {
-                
+                List(items) { item in
+                    Text(item.title)
+                }
+                .listStyle(PlainListStyle())
             }
             .navigationTitle("your tree")
             .toolbar {
@@ -37,5 +41,5 @@ struct PantryView: View {
 }
 
 #Preview {
-    PantryView(userId: "")
+    PantryView(userId: "") //this is a fake one, need to somehow make it have the real one?
 }
